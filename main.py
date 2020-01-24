244dfa85da188b9eb1829b0e2d2dc74d24d76893
Stanislav Vitko <stas@vitko.me>
2020-01-24 11:48:42 -0800

    get random rfc

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ def get_wiki_text(session):
     return head + "\n" * 2 + text
 
 
+def get_rfc_text(session):
+    while True:
+        try:
+            suggestion = random.randint(0, 9000)
+            # suggestion = 2616
+            r = session.get(f"https://www.rfc-editor.org/rfc/rfc{suggestion}.txt")
+            r.raise_for_status()
+            break # exit if everything is ok
+        except Exception:
+            print("cant' find RFC {suggestion}")
+
+    text = r.text
+    return text
+
+
+def get_random_text(session):
+    return get_rfc_text(session)
+
+
 def get_delay(mu=0.3, sigma=0.2):
     """assuming 200 characters per minute delay mu should be ~0.3"""
     return max(0, random.normalvariate(mu, sigma))
@@ -27,8 +46,9 @@ def main():
     session = HTMLSession()
 
     while True:
-        txt = get_wiki_text(session)
+        txt = get_random_text(session)
         delayed_print(txt)
+        print("\n" * 10)
 
 
 if __name__ == "__main__":
